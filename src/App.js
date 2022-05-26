d303e2bd831199a26e6acd5881c45228574936cb
Marcelo <mleandrofc@yahoo.com>
2022-05-26 15:11:28 -0300

    Login

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import LoginScreen from "./Pages/LoginScreen";
+import './App.css';
+import HomeScreen from "./Pages/HomeScreen";
+
+
+function App() {
+  return (
+    <div className="App">
+      
+       <LoginScreen/>
+      
+    </div>
+  );
+}
+
+export default App;
